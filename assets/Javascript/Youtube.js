8eacce96070e7ea45127bca32d5cdc758bd4fcb9
Roomsie Jones <seangravito@gmail.com>
2021-06-29 12:09:03 -0500

    pushing initial local history

---
+++ assets/Javascript/Youtube.js
@@ -0,0 +1,5 @@
+var YTapiKey ="AIzaSyDMLxTEPfEaHTNQDsnhf_JMyNYmurge8tE"
+var YTurlReference="https://www.googleapis.com/youtube/v3"
+
+//testing pull request
+// https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&key=AIzaSyDMLxTEPfEaHTNQDsnhf_JMyNYmurge8tE did work
---
+++ assets/Javascript/script.js
@@ -0,0 +1,21 @@
+
+// gets local storage if there is any for the searches and saves them to the variable 'searchHistory'
+var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+
+
+if (history.length > 0) {
+    PLACEHOLDERFUNCTION(history[history.length -1]);
+}
+
+
+
+// add this to the call to store the search to the history
+        .then(function(response){
+            var PLACEHOLDERsongName = response.PLACEHOLDER;
+            if (history.indexOf(SONGNAME) === -1) {
+                history.push(PLACEHOLDERHEADING);
+                localStorage.setItem("history", JSON.stringify(searchHistory));
+            }
+        }
+
+        
