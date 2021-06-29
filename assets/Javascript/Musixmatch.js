106607b1cd1ca02eca940735e99cd755baedcea7
Michael Callahan <mcallahan0@yahoo.com>
2021-06-29 13:44:32 -0400

    Fixed issue #17 making it so the lyrics will display on the page based on the search input

--- assets/Javascript/Musixmatch.js
+++ assets/Javascript/Musixmatch.js
@@ -1,5 +1,8 @@
+var lyricsText = document.getElementById('lyricsText');
+var songText = document.getElementById('search');
+
 var apiKey = '8c4828f1e437b6afe66bd85c55527db1';
-var song = '';
+var song = songText.textContent;
 var artist = '';
 
 var fetchLyrics = 'https://enigmatic-citadel-24557.herokuapp.com/' + 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=' + song + '&q_artist=' + artist + '&apikey=' + apiKey;
@@ -11,8 +14,8 @@ function getLyrics(request) {
         })
         .then(function (data) {
             console.log(data);
-            lyrics.textContent = data.message.body.lyrics.lyrics_body;
+            lyricsText.textContent = data.message.body.lyrics.lyrics_body;
         })
 }
 
-getAPI(fetchLyrics);
+getLyrics(fetchLyrics);
