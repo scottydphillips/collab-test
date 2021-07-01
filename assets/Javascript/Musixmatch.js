c0f4352aa51f104e6cfbfd95ff630105b2f8baac
Michael Callahan <mcallahan0@yahoo.com>
2021-07-01 13:31:50 -0400

    Additional functions added for new Genius API and possibly another API. If/else conditional added to the MusixMatch API

--- assets/Javascript/Musixmatch.js
+++ assets/Javascript/Musixmatch.js
@@ -1,11 +1,10 @@
 var searchBtn = document.getElementById('search');
-var lyricsModal = document.getElementById('lyrics-modal');
 var lyricsText = document.getElementById('lyrics-text');
 
 var apiKey = '8c4828f1e437b6afe66bd85c55527db1';
+var geniusApi = 'HfA6g6drdIw4BfueT9XFLsHa3oraqePJ0DEm4Lsx78zF5811JRKVurhCr1DZrS1k';
 
 searchBtn.addEventListener('click', getLyrics);
-lyricsModal.addEventListener('click', displayLyrics);
 
 function displaySong() {
     console.log('test search button');
@@ -23,16 +22,44 @@ function getLyrics(event) {
 
     var fetchLyrics = 'https://enigmatic-citadel-24557.herokuapp.com/' + 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=' + song + '&q_artist=' + artist + '&apikey=' + apiKey;
 
+
     fetch (fetchLyrics)
         .then(function (res) {
             return res.json();
         })
         .then(function (data) {
             console.log(data);
-            lyricsText.textContent = data.message.body.lyrics.lyrics_body;
-        })
-}
 
-function displayLyrics() {
-    console.log('test modal');
+            if (data.message.body.length === 0) {
+                lyricsText.textContent = "Sorry, can't find the lyrics for this song. Try searching for another one!"
+            } else {
+                lyricsText.textContent = data.message.body.lyrics.lyrics_body;
+            }
+        })
+
+    var fetchGenius = 'https://enigmatic-citadel-24557.herokuapp.com/' + 'http://api.genius.com/search?q=' + song + '&access_token=' + geniusApi;
+
+    fetch (fetchGenius)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
+    var fetchInfo = 'https://enigmatic-citadel-24557.herokuapp.com/' + 'https://shazam.p.rapidapi.com/search?term=' + song;
+    
+        fetch(fetchInfo, {
+            "method": "GET",
+            "headers": {
+            "x-rapidapi-host": "shazam.p.rapidapi.com"
+            }
+        })
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+
 }
